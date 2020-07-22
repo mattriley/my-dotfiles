b5b73e61ecdb1125eacef9a682b6e42bedab2a26
Matt Riley <mattrileyaus@gmail.com>
2020-07-22 21:12:58 +1000

    update eslint

--- install.sh
+++ install.sh
@@ -5,7 +5,10 @@ cwd=$(pwd)
 
 ln -sf $cwd/src/.bash_profile ~/.bash_profile
 ln -sf $cwd/src/.bashrc ~/.bashrc
+
 ln -sf $cwd/src/.eslintrc ~/.eslintrc
+cp $cwd/src/.eslintrc ~/home/code/agileavatars-wip/
+
 ln -sf $cwd/src/.gitconfig ~/.gitconfig
 ln -sf $cwd/src/.zshrc ~/.zshrc
 ln -sf $cwd/src/vscode-settings.json "$HOME/Library/Application Support/Code/User/settings.json"
