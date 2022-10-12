d6b6b1cab5ddf23318bd437ba33f41f500722590
Matt Riley <m@ttriley.dev>
2022-10-13 10:24:19 +1100

    Unspecified changes

--- install.sh
+++ install.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
+dotfiles="$(pwd)/dotfiles"
+
 function link {
     local source="$1"
     local dest="${2:-$1}"
-    ln -sf "$(pwd)/src/$source" "$HOME/$dest"
+    ln -sf "$dotfiles/$source" "$HOME/$dest"
 }
 
 function install {
-
-    node "sort-vscode-settings.js"
+    node "./src/sort-vscode-settings.js" "$dotfiles/vscode-settings.json";
     link "vscode-settings.json" "Library/Application Support/Code/User/settings.json"
     link ".gitconfig"    
     link ".bash_profile"
     link ".bashrc"    
     link ".zshrc"
-
 }
 
 install
