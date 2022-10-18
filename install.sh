a742708d33372859758fa16de1c359e6c4432786
Matt Riley <m@ttriley.dev>
2022-10-18 21:20:55 +1100

    Unspecified changes

--- install.sh
+++ install.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 
-dotfiles="$(pwd)/dotfiles"
-
-function link {
-    local source="$1"
-    local dest="${2:-$1}"
-    ln -sf "$dotfiles/$source" "$HOME/$dest"
-}
+targets="targets"
 
 function install {
-    node "./src/sort-vscode-settings.js" "$dotfiles/vscode-settings.json";
-    link "vscode-settings.json" "Library/Application Support/Code/User/settings.json"
-    link ".gitconfig"    
-    link ".bash_profile"
-    link ".bashrc"    
-    link ".zshrc"
+    targets="targets"
+
+    node "./src/sort-vscode-settings.js" "./$targets/Library/Application Support/Code/User/settings.json";
+
+    find "$targets" -type f -print0 | 
+        while IFS= read -r -d '' path; do 
+            echo "$(pwd)/$path" "$HOME/${path#*/}"
+            ln -sf "$(pwd)/$path" "$HOME/${path#*/}"
+        done
+
+
+    # link "vscode-settings.json" "Library/Application Support/Code/User/settings.json"
+    # link ".gitconfig"    
+    # link ".bash_profile"
+    # link ".bashrc"    
+    # link ".zshrc"
 }
 
 install
--- src/sort-vscode-settings.js
+++ src/sort-vscode-settings.js
@@ -1,5 +1,5 @@
 const [target] = require('process').argv.slice(2);
-const settings = require(target);
+const settings = JSON.parse(require('fs').readFileSync(target, 'utf-8'));
 const sortedKeys = Object.keys(settings).sort();
 const sortedEntries = sortedKeys.map(k => [k, settings[k]]);
 const sortedSettings = Object.fromEntries(sortedEntries);
