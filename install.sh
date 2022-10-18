5e5d837777a58c7415f248edf4b7a24c2caa9421
Matt Riley <m@ttriley.dev>
2022-10-18 21:21:32 +1100

    Unspecified changes

--- install.sh
+++ install.sh
@@ -1,24 +1,13 @@
 #!/bin/bash
 
-targets="targets"
-
 function install {
     targets="targets"
-
-    node "./src/sort-vscode-settings.js" "./$targets/Library/Application Support/Code/User/settings.json";
-
+    node "src/sort-vscode-settings.js" "$targets/Library/Application Support/Code/User/settings.json";
     find "$targets" -type f -print0 | 
         while IFS= read -r -d '' path; do 
             echo "$(pwd)/$path" "$HOME/${path#*/}"
             ln -sf "$(pwd)/$path" "$HOME/${path#*/}"
         done
-
-
-    # link "vscode-settings.json" "Library/Application Support/Code/User/settings.json"
-    # link ".gitconfig"    
-    # link ".bash_profile"
-    # link ".bashrc"    
-    # link ".zshrc"
 }
 
 install
