2ec0bbe55f6d03c627d7fd8f2a7c863809d4a697
Matt Riley <m@ttriley.dev>
2022-10-27 09:28:04 +1100

    Unspecified changes

---
+++ src/bash/modules/git/aliases.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# From https://github.com/ohmyzsh/ohmyzsh/blob/master/plugins/git/git.plugin.zsh
+alias gs="git status"
+alias ga="git add --all"
+alias gc="git commit -v"
+alias gp="git push"
+
+# Custom
+alias gr="git pull --rebase"
