f188b32975b5ef748c406bc8248caa989c350e37
Matt Riley <m@ttriley.dev>
2023-05-28 10:39:14 +1000

    Unspecified changes

---
+++ src/bash/modules/node/list-dep-mismatch.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+function node.list_dep_mismatch {
+
+    local package="$1"
+    local version="$2"
+    local code_dir=${3:-$CODE_DIR}
+
+    find "$code_dir" -type d -name 'node_modules' -prune -o -type f -name 'package.json' -exec node -e "require('{}').dependencies?.['$package'] !== '$version' && console.log('{}')" \;
+
+}
