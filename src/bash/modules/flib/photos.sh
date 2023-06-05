62a47d08125b6b101e9452efd330da6fc3c81364
Matt Riley <m@ttriley.dev>
2023-06-06 08:10:35 +1000

    Unspecified changes

--- src/bash/modules/flib/photos.sh
+++ src/bash/modules/flib/photos.sh
@@ -4,7 +4,8 @@ function flib.photos {
 
     local profile=${1:-$PHOTOS_DEFAULT_PROFILE}
     local photos_dir="$PHOTOS_PARTIAL_PATH$profile"
-    local node_version; node_version="$(cat "$CODE_DIR/flib/.nvmrc")"
-    cd "$photos_dir" && nvm use "$node_version"
+    cd "$photos_dir" || exit 1
+    # local node_version; node_version="$(cat "$CODE_DIR/flib/.nvmrc")"
+    # cd "$photos_dir" && nvm use "$node_version"
 
 }
--- src/bash/modules/node/list-dep-mismatch.sh
+++ src/bash/modules/node/list-dep-mismatch.sh
@@ -6,6 +6,6 @@ function node.list_dep_mismatch {
     local version="$2"
     local code_dir=${3:-$CODE_DIR}
 
-    find "$code_dir" -type d -name 'node_modules' -prune -o -type f -name 'package.json' -exec node -e "require('{}').dependencies?.['$package'] !== '$version' && console.log('{}')" \;
+    find "$code_dir" -type d -name 'node_modules' -prune -o -type f -name 'package.json' -exec node -e "const v = require('{}').dependencies?.['$package']; v && v !== '$version' && console.log('{}')" \;
 
 }
