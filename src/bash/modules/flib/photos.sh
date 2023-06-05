6fc6259d16a2bf225290dee0eeab85f64a56e1f3
Matt Riley <m@ttriley.dev>
2023-06-06 08:13:50 +1000

    Unspecified changes

--- src/bash/modules/flib/photos.sh
+++ src/bash/modules/flib/photos.sh
@@ -4,6 +4,7 @@ function flib.photos {
 
     local profile=${1:-$PHOTOS_DEFAULT_PROFILE}
     local photos_dir="$PHOTOS_PARTIAL_PATH$profile"
+    export NODENV_VERSION; NODENV_VERSION="$(cat "$CODE_DIR/flib/.node-version")"
     cd "$photos_dir" || exit 1
     # local node_version; node_version="$(cat "$CODE_DIR/flib/.nvmrc")"
     # cd "$photos_dir" && nvm use "$node_version"
